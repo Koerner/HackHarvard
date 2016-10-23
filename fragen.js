97131e7e10be728081f06ae9fbe76bfa39de3227
einahpets123 <stephanie.korbely@hotmail.com>
2016-10-22 23:10:43 -0400

    Fragen 1-5 aktualisiert

--- fragen.js
+++ fragen.js
@@ -1,25 +1,58 @@
 
 var questions = [
     {
-        "Was heißt <break time=\"0.5s\"/><phoneme alphabet=\"ipa\" ph=\"ʃɪp\">---</phoneme><break time=\"0.5s\"/> auf Deutsch?": [
-            "Schiff",
-            "Schilf",
-            "Schild",
-            "Pferd",
-            "Schnecke",
-            "Schleim"
+        "<phoneme alphabet=\"ipa\" ph=\"wɒt\">WHAT</phoneme><break time=\"0.5s\"/><phoneme alphabet=\"ipa\" ph=\"ɪz\">IS</phoneme><break time=\"0.5s\"/><phoneme alphabet=\"ipa\" ph=\"ði:\">THE</phoneme><break time=\"0.5s\"/><phoneme alphabet=\"ipa\" ph=\"ˈʤɜ:mən\">GERMAN</phoneme><break time=\"0.5s\"/><phoneme alphabet=\"ipa\" ph=\"wɜ:d\">WORD</phoneme><break time=\"0.5s\"/><phoneme alphabet=\"ipa\" ph=\"fɔ:ʳ\">FOR</phoneme><break time=\"0.5s\"/><phoneme alphabet=\"ipa\" ph=\"pleɪn\">PLANE</phoneme><break time=\"0.5s\"/>": [
+            "Flugzeug",
+            "Fahrrad",
+            "Zug",
+            "Kutsche",
+            "Auto",
+            "Schiff"
         ]
     },
 
     {
-        "Was heißt <break time=\"0.5s\"/><phoneme alphabet=\"ipa\" ph=\"θru:\">---</phoneme><break time=\"0.5s\"/> auf Deutsch?": [
-            "Pferdeäpfel.",
-            "leer.",
-            "leer.",
-            "leer.",
-            "leer.",
-            "leer."
+        "Was heißt <break time=\"0.5s\"/><phoneme alphabet=\"ipa\" ph=\"bi:tʃ\">BEACH</phoneme><break time=\"0.5s\"/> auf Deutsch?": [
+            "Strand",
+            "Wald",
+            "Wiese",
+            "Berge",
+            "Vulkan",
+            "See"
         ]
     },
 
+
+    {
+        "Was heißt <break time=\"0.5s\"/><phoneme alphabet=\"ipa\" ph=\"bi:tʃ\">PASSPORT</phoneme><break time=\"0.5s\"/> auf Deutsch?": [
+            "Reisepass",
+            "Buch",
+            "Heft",
+            "Notizen",
+            "Zettel",
+            "Personalausweis"
+        ]
+    },
 ];
+
+{
+        "Was heißt <break time=\"0.5s\"/><phoneme alphabet=\"ipa\" ph=\"bi:tʃ\">SUITCASE</phoneme><break time=\"0.5s\"/> auf Deutsch?": [
+            "Koffer",
+            "Sack",
+            "Tasche",
+            "Rucksack",
+            "Seesack",
+            "Truhe"
+        ]
+    },
+
+    {
+        "Was heißt <break time=\"0.5s\"/><phoneme alphabet=\"ipa\" ph=\"bi:tʃ\">GUIDEBOOK</phoneme><break time=\"0.5s\"/> auf Deutsch?": [
+            "Reiseführer",
+            "Taschenbuch",
+            "Notizbuch",
+            "Kochbuch",
+            "Schmonzette",
+            "Schulbuch"
+        ]
+    },
